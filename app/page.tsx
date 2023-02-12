c2c6c25a1c4435bdf23267353ff6fc58841193e3
Muhammad Usama <underusama@gmail.com>
2023-02-12 17:37:29 +0500

    prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className='text-blue-400'>Hello World</div>
+  return <div>Hello World</div>;
 }
---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  useTabs: false,
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+}
