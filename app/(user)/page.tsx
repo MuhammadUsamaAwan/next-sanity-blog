8a299b39867c347ba15da193602965c2fde7e215
Muhammad Usama <underusama@gmail.com>
2023-02-13 14:10:59 +0500

    blog list component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -4,6 +4,7 @@ import { groq } from 'next-sanity';
 import { client } from '@/lib/sanity.client';
 import PreviewSuspense from '@/components/PreviewSuspense';
 import PreviewBlogList from '@/components/PreviewBlogList';
+import BlogList from '@/components/BlogList';
 
 const query = groq`
   *[_type=='post'] {
@@ -22,13 +23,14 @@ export default async function Home() {
     );
   }
 
-  // const posts = await client.fetch(query);
+  const posts = await client.fetch(query);
   return (
     <div>
-      <div className='mb-4 flex items-center justify-center space-x-2'>
+      <div className='mb-4 flex items-center justify-center space-x-2 border-b border-white/20 pb-4'>
         <MdOutlineTrendingUp className='text-2xl' />
         <h1 className='text-2xl'>Trending on The Blog</h1>
       </div>
+      <BlogList posts={posts} />
     </div>
   );
 }
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,7 +1,44 @@
+import urlFor from '@/lib/urlFor';
+import dayjs from 'dayjs';
+import Image from 'next/image';
+import Link from 'next/link';
+
 interface Props {
   posts: any;
 }
 
 export default function BlogList({ posts }: Props) {
-  return <div>BlogList</div>;
+  return (
+    <div className='grid grid-cols-1 gap-x-10 gap-y-20 sm:grid-cols-2'>
+      {posts?.map((post: any) => (
+        <article key={post?._id} className='space-y-1 duration-200 hover:scale-[101%]'>
+          <Link href={`/${post?.slug?.current}`}>
+            <div className='relative h-80 w-full'>
+              <Image
+                className='brightness-80 rounded object-cover'
+                src={urlFor(post?.mainImage).url()}
+                alt={post?.title}
+                fill
+              />
+            </div>
+            <h1 className='text-xl'>{post?.title}</h1>
+          </Link>
+          <div className='item-center flex space-x-1'>
+            <div className='relative h-6 w-6'>
+              <Image
+                className='rounded-full object-cover'
+                src={urlFor(post?.mainImage).url()}
+                alt={posts?.author?.name}
+                fill
+              />
+            </div>
+            <Link href={`/users/${post?.author?.slug?.current}`} className='text-sm hover:underline'>
+              {post?.author?.name}
+            </Link>
+            <p className='ml-5 text-sm'>{dayjs(post?._createdAt).format('DD MMM')}</p>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 }
